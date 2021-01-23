51e1a294f88e4650885818a277aa67cdebebeb2d
Adrian Piwin <adrian7048@gmail.com>
2021-01-23 15:53:39 -0500

    Added water generation

---
+++ ShipGame3D/Assets/Scripts/Float.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Float : MonoBehaviour
+{
+    public Rigidbody rigidbody;
+    [SerializeField] private float depthBeforeSubmerged = 1f;
+    [SerializeField] private float displacementAmount = 3f;
+    [SerializeField] private float waterDrag = 0.99f;
+    [SerializeField] private float waterAngularDrag = 0.5f;
+
+    [SerializeField] private int floaterCount = 4;
+
+    void FixedUpdate()
+    {
+        rigidbody.AddForceAtPosition(Physics.gravity / floaterCount, transform.position, ForceMode.Acceleration);
+
+        float waveHeight = WaveManager.instance.GetWaveHeight(transform.position.x);
+
+        if (transform.position.y < waveHeight) {
+            float displacementMultiplayer = Mathf.Clamp01((waveHeight - transform.position.y) / depthBeforeSubmerged) * displacementAmount;
+            rigidbody.AddForceAtPosition(new Vector3(0f, Mathf.Abs(Physics.gravity.y) * displacementMultiplayer, 0f), transform.position ,ForceMode.Acceleration);
+            rigidbody.AddForce(displacementMultiplayer * -rigidbody.velocity * waterDrag * Time.fixedDeltaTime, ForceMode.VelocityChange);
+            rigidbody.AddTorque(displacementMultiplayer * -rigidbody.angularVelocity * waterAngularDrag * Time.fixedDeltaTime, ForceMode.VelocityChange);
+        }
+    }
+}
