87a1a0bdebe9fa03f2d2bc180fd673cf8aa77320
Adrian Piwin <adrian7048@gmail.com>
2021-01-23 20:23:32 -0500

    Minor changes to floating

---
+++ ShipGame3D/Assets/Scripts/BoatController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class BoatController : MonoBehaviour
+{
+    [SerializeField] private bool moveForward;
+    [SerializeField] private float speed;
+
+    private Rigidbody body;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (moveForward)
+            body.AddForce(transform.forward * speed);
+    }
+}
